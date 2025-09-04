bf289adfa7f5301b413fad79bfcf1c04575144ea
Pedro Henrique Feitosa <50965091+pedrofeitosa97@users.noreply.github.com>
2025-09-04 19:14:58 -0300

    fixed api url

--- src/shared/services/socket.ts
+++ src/shared/services/socket.ts
@@ -1,9 +1,6 @@
 import { io } from 'socket.io-client'
 
-const socket = io(
-  import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000',
-  {}
-)
+const socket = io(import.meta.env.VITE_API_BASE_URL, {})
 
 socket.connect()
 
